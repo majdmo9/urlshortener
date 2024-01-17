e0973efbb8fcdd9afcfdec1466f246b45c7baf7d
Majd Mousa <majdmousa@Majds-MacBook-Pro-2.local>
2024-01-17 17:51:00 +0200

    change env vars

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    DATABASE_URL: "mysql://johndoe:randompassword@localhost:3306/mydb",
-    NEXT_PUBLIC_API_URL: "http://localhost:3000",
-  },
+  env: {},
 };
 
 module.exports = nextConfig;
